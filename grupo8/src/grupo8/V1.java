52c7bf0a9f92abc8ec84b874c60587c57589aa5e
jasiel <jasiel.adrianzenv@istpargentina.edu.pe>
2026-04-19 11:16:36 -0500

    diseño

--- grupo8/src/grupo8/V1.java
+++ grupo8/src/grupo8/V1.java
@@ -6,11 +6,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class V1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JTextField txtId;
+	private JTextField txtProd;
+	private JTextField txtPrec;
+	private JTextField txtStock;
+	private JButton btnInsertar;
+	private JButton btnModificar;
+	private JButton btnEliminar;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -38,10 +53,70 @@ public class V1 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("PRUEBA");
-		lblNewLabel.setBounds(10, 10, 44, 12);
-		contentPane.add(lblNewLabel);
+		{
+			lblNewLabel = new JLabel("ID");
+			lblNewLabel.setBounds(20, 10, 44, 12);
+			contentPane.add(lblNewLabel);
+		}
+		{
+			lblNewLabel_1 = new JLabel("Producto");
+			lblNewLabel_1.setBounds(20, 32, 44, 12);
+			contentPane.add(lblNewLabel_1);
+		}
+		{
+			lblNewLabel_2 = new JLabel("Precio");
+			lblNewLabel_2.setBounds(20, 54, 44, 12);
+			contentPane.add(lblNewLabel_2);
+		}
+		{
+			lblNewLabel_3 = new JLabel("Stock");
+			lblNewLabel_3.setBounds(20, 76, 44, 12);
+			contentPane.add(lblNewLabel_3);
+		}
+		{
+			txtId = new JTextField();
+			txtId.setBounds(83, 7, 96, 18);
+			contentPane.add(txtId);
+			txtId.setColumns(10);
+		}
+		{
+			txtProd = new JTextField();
+			txtProd.setBounds(83, 29, 96, 18);
+			contentPane.add(txtProd);
+			txtProd.setColumns(10);
+		}
+		{
+			txtPrec = new JTextField();
+			txtPrec.setBounds(83, 51, 96, 18);
+			contentPane.add(txtPrec);
+			txtPrec.setColumns(10);
+		}
+		{
+			txtStock = new JTextField();
+			txtStock.setBounds(83, 73, 96, 18);
+			contentPane.add(txtStock);
+			txtStock.setColumns(10);
+		}
+		{
+			btnInsertar = new JButton("Insertar");
+			btnInsertar.setBounds(207, 6, 84, 20);
+			contentPane.add(btnInsertar);
+		}
+		{
+			btnModificar = new JButton("Modificar");
+			btnModificar.setBounds(207, 28, 84, 20);
+			contentPane.add(btnModificar);
+		}
+		{
+			btnEliminar = new JButton("Eliminar");
+			btnEliminar.setBounds(207, 50, 84, 20);
+			contentPane.add(btnEliminar);
+		}
+		{
+			textArea = new JTextArea();
+			textArea.setBounds(21, 110, 405, 143);
+			contentPane.add(textArea);
+		}
 
 	}
 }
